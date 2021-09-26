c054459eed9a404ffcdf4fa47a1066df55fc16fc
saifulislammunna <saifulmunna28@gmail.com>
2021-09-26 23:28:51 +0600

    my-checklist-added

---
+++ my-checklist.js
@@ -0,0 +1,142 @@
+0.
+//  comment korte pari
+/* multiple line comment korte pari */
+1.
+//  var myNumber = 45;
+//  var myString = 'saiful';
+//  var theSun = true;
+
+ 2.
+//  let saiful = 45;
+//  const jannnat = "givenInAkhirat"
+
+//  saiful = 12;
+//  console.log(saiful)
+
+
+ 3. /* same process a addition,bijog,multiplicaton,vag,vagsesh kora jabe */
+//  function addTwoNumbers(num1,num2){
+//      console.log(num1,num2);
+//      var total = num1 + num2;
+//      return total;
+//  }
+//  var numOne = 34;
+//  var numTwo = 50; 
+//  var addition = addTwoNumbers(numTwo, numOne);
+//  console.log('code dea math kortece', addition);
+
+ 4.
+//  var miniketRicePrice = 65;
+//  var irfanRicePrice = 62;
+
+// if(miniketRicePrice<irfanRicePrice){
+//     console.log('moja kore khamu');
+// }
+// else if(miniketRicePrice>irfanRicePrice){
+//     console.log('kopale miniket acce');
+// }
+// else if(miniketRicePrice==irfanRicePrice){
+//     console.log('kopale maire acce');
+// }
+//  if(miniketRicePrice!=irfanRicePrice){
+//     console.log('kopale kichu nai');
+// }
+/*  if(miniketRicePrice <= irfanRicePrice){
+    console.log('kopale miniket acce');
+}
+else if(miniketRicePrice>=irfanRicePrice){
+    console.log('kopale irfan acce');
+}
+ */
+
+5.
+5.1
+// var haveJob = true;
+// var savingMoney = 200000;
+
+// if(haveJob && savingMoney){
+//     console.log('biye kore falai');
+// }
+ 
+
+5.2
+// var haveJob = false;
+// var savingMoney = 2000000;
+// var hasHouse = true;
+
+// if((haveJob && savingMoney) || hasHouse){
+//     console.log('biye kore falai2');
+// }
+
+6.
+// var haveJob = false;
+// var savingMoney = 200000;
+
+// if(haveJob && savingMoney){
+//     console.log('biye kore falai');
+// }
+// else{
+//     console.log('tor kopale biya nai');
+// }
+
+7.
+// var numbers = 7;
+// while(numbers<=19){
+//     console.log(numbers);
+//     numbers = numbers + 2;
+// }
+
+8.
+// var numbers = [1,2,3,4,5,56];
+// numbers[4]= 15;
+// console.log(numbers);
+
+// numbers.push(555);
+// numbers.push(555999);
+// console.log(numbers);
+
+// numbers.pop();
+// console.log(numbers);
+
+9.
+
+// var numbers = [1,2,3,4,5,56];
+
+// for(i=0;i<numbers.length;i++){
+//     var number = numbers[i];
+//     console.log(number);
+// }
+
+10.
+// var numbers = [50,81,63,94,55,56];
+// for(i=0;i<numbers.length;i++){
+//     if(numbers[i]>80){
+//         var numbers = [];
+//         console.log(numbers);
+//     }
+// }
+
+11.
+// function addTwoNumbers(num1,num2,num3){
+//     console.log(num1,num2,num3);
+//     var multiplication = num1 * num2 *num3;
+//     return multiplication;
+// }
+// var numOne = 34;
+// var numTwo = 50; 
+// var numThree = 60;
+// var multiply = addTwoNumbers(numTwo, numOne, numThree);
+// console.log('code dea math kortece', multiply);
+12.
+var computer ={
+        price: 29000,
+        storage: '256gb',
+        color: 'silver',
+        processor: 'intel i5'
+}
+
+var computerProcessor = computer.processor;
+computer.processor = 'intel i10'
+console.log(computer);
+
+
---
+++ problem-solving-checklist.js
@@ -0,0 +1,19 @@
+//1.conversion
+feetToInch
+
+//2.conversion
+centimeterToMeter
+
+
+//3.calculation
+pageRequirements
+
+//book1 has 100 pages
+//book2 has 200 pages
+//book3 has 300 pages
+
+//4.friens
+bestFriend 
+
+//5.will stop the loop if the array has any negative numnber and returns all the positive number before the negative number
+onlypositive 
